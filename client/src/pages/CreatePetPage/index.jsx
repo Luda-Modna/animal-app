5f0c3154d446450afe6c9eb9f5592e702c7a1e84
Luda Modna <ludmilamodna3@gmail.com>
2025-11-01 13:34:53 +0200

    Add base structure react app

---
+++ client/src/pages/CreatePetPage/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function CreatePetPage () {
+  return <div>CreatePet</div>;
+}
+
+export default CreatePetPage;
---
+++ client/src/pages/PetsListPage/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function PetsListPage () {
+  return <div>PetsList</div>;
+}
+
+export default PetsListPage;
