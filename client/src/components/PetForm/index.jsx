8cb9985bac6434675d50225d30201e8fd89162c3
Luda Modna <ludmilamodna3@gmail.com>
2025-11-01 16:57:13 +0200

    Fix form

--- client/src/components/PetForm/index.jsx
+++ client/src/components/PetForm/index.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { Form, Formik, Field, ErrorMessage } from 'formik';
+import { connect } from 'react-redux';
 import CONSTANTS from '../../constants';
 
-function PetForm () {
+function PetForm ({ petTypes }) {
   const initialValues = {
     name: '',
     owner: '',
@@ -10,10 +11,10 @@ function PetForm () {
     description: '',
     city: CONSTANTS.CITIES[0],
     lostDate: '',
-    petTypeId: '1',
+    petTypeId: petTypes[0]?.id ?? '',
   };
   const handleSubmit = (values, formikBag) => {
-    console.log(values)
+    console.log(values);
     formikBag.resetForm();
   };
   return (
@@ -58,20 +59,24 @@ function PetForm () {
               ))}
             </select>
           </label>
-          <label>
-            Pet's type:
-            <select
-              name='petTypeId'
-              value={formikProps.values.petTypeId}
-              onChange={formikProps.handleChange}
-            >
-              {[{ id: 1, type: 'parrot' }].map(t => (
-                <option key={t.id} value={t.id}>
-                  {t.type}
-                </option>
-              ))}
-            </select>
-          </label>
+          {petTypes.length !== 0 && (
+            <>
+              <label>
+                Pet's type:
+                <select
+                  name='petTypeId'
+                  value={formikProps.values.petTypeId}
+                  onChange={formikProps.handleChange}
+                >
+                  {petTypes.map(t => (
+                    <option key={t.id} value={t.id}>
+                      {t.type}
+                    </option>
+                  ))}
+                </select>
+              </label>
+            </>
+          )}
           <button type='submit'>Add Pet</button>
         </Form>
       )}
@@ -79,4 +84,6 @@ function PetForm () {
   );
 }
 
-export default PetForm;
+const mapStateToProps = ({ petsData: { petTypes } }) => ({ petTypes });
+
+export default connect(mapStateToProps)(PetForm);
