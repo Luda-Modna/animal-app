f2c37fb5ed652d4cf907967a01acd042cfe6048e
Luda Modna <ludmilamodna3@gmail.com>
2025-11-01 12:11:25 +0200

    Add pet types seed

---
+++ server/seeders/20251101092259-add-types.js
@@ -0,0 +1,34 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.bulkInsert(
+      'pet_types',
+      [
+        {
+          type: 'cat',
+          created_at: new Date(),
+          updated_at: new Date(),
+        },        {
+          type: 'dog',
+          created_at: new Date(),
+          updated_at: new Date(),
+        },        {
+          type: 'pig',
+          created_at: new Date(),
+          updated_at: new Date(),
+        },        {
+          type: 'chipmunk',
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+      ],
+      {}
+    );
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('pet_types', null, {});
+  },
+};
