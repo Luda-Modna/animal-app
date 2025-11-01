5537393445fb0d393ad17fb4e7133837f8451a9c
Luda Modna <ludmilamodna3@gmail.com>
2025-11-01 13:47:23 +0200

    Add router

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,36 @@
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import CreatePetPage from './pages/CreatePetPage';
+import PetsListPage from './pages/PetsListPage';
+
 function App () {
-  return
+  return (
+    <Router>
+      <nav>
+        <ul>
+          <li>
+            <Link to='/'>Home</Link>
+          </li>
+          <li>
+            <Link to='/pet/create'>Add Pet</Link>
+          </li>
+          <li>
+            <Link to='/pets'>Pet List</Link>
+          </li>
+        </ul>
+      </nav>
+      <Switch>
+        <Route path='/' exact>
+          Home
+        </Route>
+        <Route path='/pet/create'>
+          <CreatePetPage />
+        </Route>
+        <Route path='/pets'>
+          <PetsListPage />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
