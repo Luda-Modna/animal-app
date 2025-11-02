a9e3f074162cd556bd6982a2538ff0bba3157c6e
Luda Modna <ludmilamodna3@gmail.com>
2025-11-02 17:18:34 +0200

    Add running cat

---
+++ client/src/components/RunningCat/index.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./RunningCat.module.sass";
+
+const RunningCat = () => {
+  return <div className={styles.cat}></div>;
+};
+
+export default RunningCat;
