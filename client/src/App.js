d97d2e45fbbfa3c703e3077397196df27dda30d9
Luda Modna <ludmilamodna3@gmail.com>
2025-11-02 17:16:40 +0200

    Add app styles

--- client/src/App.js
+++ client/src/App.js
@@ -1,35 +1,32 @@
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Home from './pages/Home';
 import CreatePetPage from './pages/CreatePetPage';
 import PetsListPage from './pages/PetsListPage';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import styles from './App.module.sass';
 
 function App () {
   return (
-    <Router>
-      <nav>
-        <ul>
-          <li>
-            <Link to='/'>Home</Link>
-          </li>
-          <li>
-            <Link to='/pet/create'>Add Pet</Link>
-          </li>
-          <li>
-            <Link to='/pets'>Pet List</Link>
-          </li>
-        </ul>
-      </nav>
-      <Switch>
-        <Route path='/' exact>
-          Home
-        </Route>
-        <Route path='/pet/create'>
-          <CreatePetPage />
-        </Route>
-        <Route path='/pets'>
-          <PetsListPage />
-        </Route>
-      </Switch>
-    </Router>
+    <div className={styles.layout}>
+      <Router>
+        <Header />
+        <Switch>
+          <main className={styles.main}>
+            <Route path='/' exact>
+              <Home />
+            </Route>
+            <Route path='/pet/create'>
+              <CreatePetPage />
+            </Route>
+            <Route path='/pets'>
+              <PetsListPage />
+            </Route>
+          </main>
+        </Switch>
+      </Router>
+      <Footer />
+    </div>
   );
 }
 
